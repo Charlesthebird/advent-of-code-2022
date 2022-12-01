e0a2fd84cfacc6d0f0b0865f54f836f545bb7d64
Nicholas Bucher <behappy54321@gmail.com>
2022-12-01 12:40:46 -0500

    day 1 complete!

--- out/01.js
+++ out/01.js
@@ -1,5 +1,22 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-var inputTxt = fs.readFileSync("./inputs/01.txt", "utf-8");
+var inputStr = fs.readFileSync("./inputs/01.txt", "utf-8");
+var result = inputStr
+    .split("\n")
+    .reduce(function (p, c) {
+    return c.trim() === ""
+        ? __spreadArray(__spreadArray([], p, true), [0], false) : __spreadArray(__spreadArray([], p.slice(0, -1), true), [p.at(-1) + Number.parseInt(c)], false);
+}, [0])
+    .reduce(function (p, c) { return (c > p ? c : p); }, 0);
+console.log(result);
 //# sourceMappingURL=01.js.map
--- src/01.ts
+++ src/01.ts
@@ -1,3 +1,16 @@
 import * as fs from "fs";
-
 const inputStr = fs.readFileSync("./inputs/01.txt", "utf-8");
+
+// https://adventofcode.com/2022/day/1
+const result = inputStr
+  .split("\n")
+  .reduce(
+    (p, c) =>
+      c.trim() === ""
+        ? [...p, 0]
+        : [...p.slice(0, -1), p.at(-1) + Number.parseInt(c)],
+    [0]
+  )
+  .reduce((p, c) => (c > p ? c : p), 0);
+
+console.log(result);
